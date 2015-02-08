0d5d8958d351e9aa8b6b1c03b2cdde9406961196
Piotr Wittchen <piotr@wittchen.biz.pl>
2015-02-08 14:25:44 +0100

    reformatting code

--- OptimizeDatabase.php
+++ OptimizeDatabase.php
@@ -7,43 +7,35 @@ class OptimizeDatabase {
 	private $db_name;
 	private $connection;
 
-	public function set_connection_config($host, $user, $password, $db_name)
-	{
+	public function set_connection_config($host, $user, $password, $db_name) {
 		$this->host = $host;
 		$this->user = $user;
 		$this->password = $password;
 		$this->db_name = $db_name;
 	}
 
-	private function connect()
-	{
+	private function connect() {
 		$this->connection = mysql_connect($this->host,$this->user,$this->password);
 
-		if(!$this->connection)
-		{
+		if(!$this->connection) {
 			throw new Exception('Error: cannot connect to database.');
 		}
 
-		if(!mysql_select_db($this->db_name))
-		{
+		if(!mysql_select_db($this->db_name)) {
 			throw new Exception('Error cannot select database with name: '.$this->db_name.'.');
 		}
 	}
 
-	private function disconnect()
-	{
+	private function disconnect() {
 		mysql_close($this->connection);
 	}
 
-	private function mysql_fetch_all($query)
-	{
+	private function mysql_fetch_all($query) {
 		$result = array();
 		$executed_query = mysql_query($query);
 
-		if(mysql_num_rows($executed_query))
-		{
-			while($row = mysql_fetch_array($executed_query))
-			{
+		if(mysql_num_rows($executed_query)) {
+			while($row = mysql_fetch_array($executed_query)) {
 				$result[$row['Name']] = $row;
 			}
 		}
@@ -51,44 +43,34 @@ class OptimizeDatabase {
 		return $result;
 	}
 
-	private function get_tables()
-	{
+	private function get_tables() {
 		$this->connect();
 		$result = $this->mysql_fetch_all('SHOW TABLE STATUS');
 		return $result;
 	}
 
-	private function get_overloaded_tables()
-	{
+	private function get_overloaded_tables() {
 		$tables = $this->get_tables();
 
-		foreach($tables as $table => $value)
-		{
-			if($value['Data_free'] > 0)
-			{
+		foreach($tables as $table => $value) {
+			if($value['Data_free'] > 0) {
 				$result[$table] = $value;
 			}
 		}
 
-	return $result;
+		return $result;
 	}
 
-	public function do_optimization()
-	{
+	public function do_optimization() {
 		$array = $this->get_overloaded_tables();
 		$count = count($array);
 
-		if($count > 0)
-		{
-			foreach($array as $table => $value)
-			{
+		if($count > 0) {
+			foreach($array as $table => $value) {
 				$data_free += $value['Data_free'];
 				mysql_query('OPTIMIZE TABLE `'.$table.'`');
 			}
-		}
-
-		else
-		{
+		} else {
 			$data_free = 0;
 		}
 
--- OptimizeDatabases.php
+++ OptimizeDatabases.php
@@ -6,51 +6,43 @@ class OptimizeDatabases {
 	private $file_with_databases;
 	private $optimization_log;
 
-	public function __construct($host = 'localhost', $file_with_databases = 'dbs.php', $optimization_log = 'optimization.log')
-	{
+	public function __construct($host = 'localhost', $file_with_databases = 'dbs.php', $optimization_log = 'optimization.log') {
 		$this->optimize_database = new optimize_database;
 		$this->host = $host;
 		$this->dbs_file = $file_with_databases;
 		$this->optimization_log = $optimization_log;
 	}
 
-	private function get_dabases_from_file($filename, $separator = '|')
-	{
-		if(file_exists($filename))
-		{
+	private function get_dabases_from_file($filename, $separator = '|') {
+		if(file_exists($filename)) {
 			$file  = file($filename);
 			$lines = count($file);
 
-			for ($i = 0; $i < $lines; $i++)
-			{
+			for ($i = 0; $i < $lines; $i++) {
 				$line = explode($separator, trim($file[$i]));
 				$file[$i] = $line;
 			}
 
-		$file = array_slice($file, 1, -1);
-		return $file;
+			$file = array_slice($file, 1, -1);
+			return $file;
 		}
 	}
 
-	private function count_databases()
-	{
+	private function count_databases() {
 		return count(file($this->dbs_file)) - 2;
 	}
 
-	public function do_optimization($log = true)
-	{
+	public function do_optimization($log = true) {
 		$array = $this->get_dabases_from_file($this->dbs_file);
 
-		for($i = 0, $count = count($array) - 1; $i <= $count; $i++)
-		{
+		for($i = 0, $count = count($array) - 1; $i <= $count; $i++) {
 			$this->optimize_database->set_connection_config($this->host, $array[$i][0], $array[$i][1], $array[$i][2]);
 			$optimization = $this->optimize_database->do_optimization();
 			$data_free += $optimization['data_free'];
 			$overloaded_tables += $optimization['overloaded_tables'];
 		}
 
-		if($log)
-		{
+		if($log) {
 			$file = fopen($this->optimization_log,'a+');
 			fwrite($file, date('Y-m-d, H:i:s').' - optimized databases: '.$this->count_databases().', optimized '.$overloaded_tables.' tables, retrieved '.$data_free." bytes\n");
 			fclose($file);
